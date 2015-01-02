23aca0e92e1192a8f52a8afd2b577f9aa5d1b6ba
Armando Tresova <xjsv24@gmail.com>
2015-01-02 15:24:09 -0500

    Changed name & added image file
    Changed name & added image file for BBLog upload.

--- hide-buy-battlepacks.js
+++ hide-buy-battlepacks.js
@@ -2,16 +2,16 @@
  * Remove "BUY BATTLEPACKS" box:
  *  - ideas by Armand Tresova
  *
- * @author atresova
- * @version 1.0.0
+ * @author xjsv
+ * @version 1.0.1
  * @url https://raw.githubusercontent.com/XjSv/BBLog-Remove-Buy-BattlePacks/master/hide-buy-battlepacks.js
- * @last-edit 1. 1. 2015 18:12
+ * @last-edit 1.2.2015 15:22
  */
 
 BBLog.handle("add.plugin", {
 
     id : "remove-buy-battlepacks",
-    name : "Remove 'BUY BATTLEPACKS' box",
+    name : "Remove BUY BATTLEPACKS box",
 
     configFlags : [
         ["option.remove-buy-battlepacks", 0],
@@ -19,10 +19,10 @@ BBLog.handle("add.plugin", {
 
     translations : {
         "en" : {
-            "option.remove-buy-battlepacks" : "Remove 'BUY BATTLEPACKS' box?",
+            "option.remove-buy-battlepacks" : "Remove BUY BATTLEPACKS box?",
         },
         "de" : {
-            "option.remove-buy-battlepacks" : "Remove 'BUY BATTLEPACKS' box?",
+            "option.remove-buy-battlepacks" : "Remove BUY BATTLEPACKS box?",
         }
     },
 
