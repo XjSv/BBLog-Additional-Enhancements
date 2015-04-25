d87595924f6432b536b09c3bc5ff9a13d12c538a
Armando Tresova <xjsv24@gmail.com>
2015-04-25 17:10:41 -0400

    Hide Battlefield Hardline Advertisement
    Added functionality that removes the Battlefield Hardline advertisement
    (BUY NOW) in the landing page (battlelog.battlefield.com/bf4/).

--- additional-enhancements.js
+++ additional-enhancements.js
@@ -3,9 +3,9 @@
  *  - ideas by Armand Tresova
  *
  * @author xjsv
- * @version 1.2.1
+ * @version 1.3.0
  * @url https://raw.githubusercontent.com/XjSv/BBLog-Additional-Enhancements/master/additional-enhancements.js
- * @last-edit 2.22.2015 01:00
+ * @last-edit 4.25.2015 17:10
  */
 
 BBLog.handle("add.plugin", {
@@ -17,6 +17,7 @@ BBLog.handle("add.plugin", {
         ["option.remove-buy-battlepacks", 0],
         ["option.remove-tiles",           0],
         ["option.remove-side-bar",        0],
+        ["option.remove-buy-hardline",    0],
     ],
 
     translations : {
@@ -27,6 +28,8 @@ BBLog.handle("add.plugin", {
             "option.remove-tiles.tooltip"           : "Removes the tiles below the 'TOP STORY' in the landing page (battlelog.battlefield.com/bf4/).",
             "option.remove-side-bar"                : "Remove the right sidebar in the landing page",
             "option.remove-side-bar.tooltip"        : "Removes the right sidebar in the landing page (battlelog.battlefield.com/bf4/) and expands the battle feed.",
+            "option.remove-buy-hardline"            : "Remove Battlefield Hardline Ad (BUY NOW) in the landing page",
+            "option.remove-buy-hardline.tooltip"    : "Removes Battlefield Hardline advertisement (BUY NOW) in the landing page (battlelog.battlefield.com/bf4/).",
         },
         "de" : {
             "option.remove-buy-battlepacks"         : "Remove the 'BUY BATTLEPACKS' box in the Battlepacks page",
@@ -35,6 +38,8 @@ BBLog.handle("add.plugin", {
             "option.remove-tiles.tooltip"           : "Removes the tiles below the 'TOP STORY' in the landing page (battlelog.battlefield.com/bf4/).",
             "option.remove-side-bar"                : "Remove the right sidebar in the landing page",
             "option.remove-side-bar.tooltip"        : "Removes the right sidebar in the landing page and expands the battle feed.",
+            "option.remove-buy-hardline"            : "Remove Battlefield Hardline Ad (BUY NOW) in the landing page",
+            "option.remove-buy-hardline.tooltip"    : "Removes Battlefield Hardline advertisement (BUY NOW) in the landing page (battlelog.battlefield.com/bf4/).",
         },
     },
 
@@ -43,6 +48,7 @@ BBLog.handle("add.plugin", {
           instance.removeBuyBattlePacks(instance);
           instance.removeTiles(instance);
           instance.removeSideBar(instance);
+          instance.removeBuyHardline(instance);
         }
     },
 
@@ -51,6 +57,7 @@ BBLog.handle("add.plugin", {
           instance.removeBuyBattlePacks(instance);
           instance.removeTiles(instance);
           instance.removeSideBar(instance);
+          instance.removeBuyHardline(instance);
         }
     },
 
@@ -78,6 +85,13 @@ BBLog.handle("add.plugin", {
             }
 
             $(".main-side-column").remove();
+        }
+    },
+
+    removeBuyHardline : function(instance) {
+        var path = window.location.pathname;
+        if(instance.storage("option.remove-buy-hardline") && path == '/bf4/' && $("#main-header .get-bfh-tile").length > 0) {
+            $('#main-header .get-bfh-tile').remove();
         }
     },
 });
