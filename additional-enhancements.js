945a6c525362c275188979fe194cb7e701192167
Sil3ntStorm <Sil3ntStorm@users.noreply.github.com>
2016-02-08 01:32:56 +0100

    More Hardline junk on the landing page.

--- additional-enhancements.js
+++ additional-enhancements.js
@@ -5,7 +5,7 @@
  * @author xjsv
  * @version 1.5.0
  * @url https://raw.githubusercontent.com/XjSv/BBLog-Additional-Enhancements/master/additional-enhancements.js
- * @last-edit 12.29.2015 11:30
+ * @last-edit 02.08.2015 01:30
  */
 
 BBLog.handle("add.plugin", {
@@ -33,8 +33,8 @@ BBLog.handle("add.plugin", {
 			"option.remove-tiles.tooltip": "Removes the tiles below the 'TOP STORY' in the landing page (battlelog.battlefield.com/bf4/).",
 			"option.remove-side-bar": "Remove the right sidebar in the landing page",
 			"option.remove-side-bar.tooltip": "Removes the right sidebar in the landing page (battlelog.battlefield.com/bf4/) and expands the battle feed.",
-			"option.remove-buy-hardline": "Remove Battlefield Hardline Ad (BUY NOW) in the landing page",
-			"option.remove-buy-hardline.tooltip": "Removes Battlefield Hardline advertisement (BUY NOW) in the landing page (battlelog.battlefield.com/bf4/).",
+			"option.remove-buy-hardline": "Remove Battlefield Hardline Ads",
+			"option.remove-buy-hardline.tooltip": "Removes Battlefield Hardline advertisements from the landing page (battlelog.battlefield.com/bf4/).",
 			"option.remove-comcenter": "Remove the Com Center panel on the right",
 			"option.remove-comcenter.tooltip": "Removed the Com Center on the right of all BF4 Battlelog Pages and widens the Top bar.",
 			"option.remove-app-promotion": "Remove app promotion advertisement",
@@ -54,7 +54,7 @@ BBLog.handle("add.plugin", {
 			"option.remove-side-bar": "Entferne rechte Seitenleiste auf der Startseite",
 			"option.remove-side-bar.tooltip": "Entfernt die rechte Seitenleiste auf der Startseite und verbreitert den Battlefeed.",
 			"option.remove-buy-hardline": "Entferne Hardline Werbung",
-			"option.remove-buy-hardline.tooltip": "Entfernt den 'Hardline kaufen' Werbeblock von der Startseite (battlelog.battlefield.com/bf4/).",
+			"option.remove-buy-hardline.tooltip": "Entfernt diverse Hardline Werbung von der Startseite (battlelog.battlefield.com/bf4/).",
 			"option.remove-comcenter": "Entferne Com Center",
 			"option.remove-comcenter.tooltip": "Entfernt das ComCenter am rechten Bildschirmrand auf allen (BF4) Seiten im Battlelog und passt die Breite an.",
 			"option.remove-app-promotion": "Entferne Battlelog App Werbung",
@@ -73,7 +73,7 @@ BBLog.handle("add.plugin", {
 			instance.removeBuyBattlePacks(instance);
 			instance.removeTiles(instance);
 			instance.removeSideBar(instance);
-			instance.removeBuyHardline(instance);
+			instance.removeHardlineAds(instance);
 			instance.removeComCenter(instance);
 			instance.removeCookieBar(instance);
 			instance.removeBlockedServers(instance);
@@ -87,7 +87,7 @@ BBLog.handle("add.plugin", {
 			instance.removeBuyBattlePacks(instance);
 			instance.removeTiles(instance);
 			instance.removeSideBar(instance);
-			instance.removeBuyHardline(instance);
+			instance.removeHardlineAds(instance);
 			instance.removeComCenter(instance);
 			instance.removeCookieBar(instance);
 			instance.removeBlockedServers(instance);
@@ -123,10 +123,16 @@ BBLog.handle("add.plugin", {
 		}
 	},
 
-	removeBuyHardline: function (instance) {
+	removeHardlineAds: function (instance) {
 		var path = window.location.pathname;
-		if (instance.storage("option.remove-buy-hardline") && path == '/bf4/' && $("#main-header .get-bfh-tile").length > 0) {
-			$('#main-header .get-bfh-tile').remove();
+		if (instance.storage("option.remove-buy-hardline") && path == '/bf4/') {
+			if ($("#main-header .get-bfh-tile").length > 0)
+				$('#main-header .get-bfh-tile').remove();
+			if ($('div[data-advertisement]').length > 0) {
+				$('div[data-advertisement]').each(function () {
+					$(this).remove();
+				});
+			}
 		}
 	},
 
